24e393006b2fe947f2530c78d82ad4e32f5124f2
9Mickle <zaks-00@mail.ru>
2021-02-26 20:34:48 +0300

    final

--- FirstTask/src/Converter.java
+++ FirstTask/src/Converter.java
@@ -4,7 +4,6 @@ public class Converter {
 
     public static void main(String[] args) {
 
-
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Введите размер файла в байтах: ");
