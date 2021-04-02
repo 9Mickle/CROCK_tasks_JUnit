5c73f1a083d9d81ebc27ab98b305358b5afaa1ab
9Mickle <zaks-00@mail.ru>
2021-04-02 21:25:30 +0300

    5 Задача вроде закончил с тестами

--- FifthTask/src/test/java/ru/croc/java/school/task5/managers/TestTaskManager.java
+++ FifthTask/src/test/java/ru/croc/java/school/task5/managers/TestTaskManager.java
@@ -10,19 +10,56 @@ import java.util.HashMap;
 public class TestTaskManager {
 
     Task task;
+    HashMap<String, Task> map = new HashMap<>();
 
     //Создаем экземпляр задачи.
     @BeforeEach
     public void addTask() {
         task = new Task("123", "Тест", "Для теста", "JUnit", "В процессе");
+        map.put("123", task);
+    }
+
+    public String editTaskStatus() {
+        task.setStatusTask("Решена");
+        return task.getStatusTask();
+    }
+
+    public String editTaskTitle() {
+        task.setTitle("Аквариум");
+        return task.getTitle();
+    }
+
+    public String editTaskExecutor() {
+        task.setExecutor("Рыба");
+        return task.getExecutor();
+    }
+
+    public String editTaskDescription() {
+        task.setDescription("Рыба будет плавать в аквариуме");
+        return task.getDescription();
     }
 
     @Test
-    public void editTask() {
-        task.setStatusTask("Решена");
-        Assertions.assertEquals("{123=Задача{Код='123', Наименование='Тест', Описание='Для теста', Исполнитель='JUnit', Статус=Решена}}", editTask());
+    public void testSetStatus() {
+        editTaskStatus();
+        Assertions.assertEquals(map.get("123").getStatusTask(), editTaskStatus());
     }
 
+    @Test
+    public void testSetTitle() {
+        editTaskTitle();
+        Assertions.assertEquals(map.get("123").getTitle(), editTaskTitle());
+    }
 
+    @Test
+    public void testSetExecutor() {
+        editTaskExecutor();
+        Assertions.assertEquals(map.get("123").getExecutor(), editTaskExecutor());
+    }
 
+    @Test
+    public void testSetDescription() {
+        editTaskDescription();
+        Assertions.assertEquals(map.get("123").getDescription(), editTaskDescription());
+    }
 }
